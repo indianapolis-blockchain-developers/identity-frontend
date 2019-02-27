562d8caf07f92ef45da023d870328de00ecc731a
gwenf <gwenfaraday@gmail.com>
2019-02-26 23:13:00 -0500

    added argon reference

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 
 import App from './App.vue';
+import Argon from "./plugins/argon-kit";
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 import './assets/sass/index.sass';
 
+Vue.use(Argon);
 Vue.config.productionTip = false;
 
 new Vue({
