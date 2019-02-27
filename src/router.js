fb9acfeb1c8dcfe1afdff564c2f9680ec598bfc6
gwenf <gwenfaraday@gmail.com>
2019-02-26 23:03:09 -0500

    first commit

--- src/router.js
+++ src/router.js
@@ -5,19 +5,86 @@ import Home from './views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
+  linkExactActiveClass: "active",
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: "/",
+      name: "home",
+      components: {
+        header: AppHeader,
+        default: Components,
+        footer: AppFooter
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: "/login",
+      name: "login",
+      components: {
+        header: AppHeader,
+        default: Login,
+        footer: AppFooter
+      }
     },
+    {
+      path: "/register",
+      name: "register",
+      components: {
+        header: AppHeader,
+        default: Register,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      components: {
+        header: AppHeader,
+        default: Profile,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      components: {
+        header: AppHeader,
+        default: Profile,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/relationships",
+      name: "relationships",
+      components: {
+        header: AppHeader,
+        default: Profile,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/credentials",
+      name: "credentials",
+      components: {
+        header: AppHeader,
+        default: Profile,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/messages",
+      name: "messages",
+      components: {
+        header: AppHeader,
+        default: Profile,
+        footer: AppFooter
+      }
+    }
   ],
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
